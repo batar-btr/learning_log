a9e09c5181fea5ce3b59b51d6ac72342cff76a82
batar-btr <btrn86@gmail.com>
2025-10-30 18:02:24 +0200

    feat: add single topic page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # All topics page
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
