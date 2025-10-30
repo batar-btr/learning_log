415d7323a59223dea8d46e02edc5754ad489cdd3
batar-btr <btrn86@gmail.com>
2025-10-30 14:57:37 +0200

    feat: add page templates

---
+++ learning_logs/urls.py
@@ -0,0 +1,12 @@
+"""Define URL patterns for learning logs"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    # Main Page
+    path('', views.index, name='index')
+]
